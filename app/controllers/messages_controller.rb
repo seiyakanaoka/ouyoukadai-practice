ee4cd7818ccaf94e76f4fec12b3d8de65f83e29e
seiyakanaoka <ttf.1998.seiya@gmail.com>
2021-04-07 14:09:15 +0000

    応用課題の復習

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,5 @@
 class MessagesController < ApplicationController
+  before_action :authenticate_user!, :only => [:create]
 
   def create
     if Entry.where(user_id: current_user.id, room_id: params[:message][:room_id]).present?
@@ -6,6 +7,6 @@ class MessagesController < ApplicationController
     else
       flash[:alert] = "メッセージの送信に失敗しました"
     end
-    redirect_to room_path(@message.room_id)
+    # redirect_to room_path(@message.room_id)
   end
 end
